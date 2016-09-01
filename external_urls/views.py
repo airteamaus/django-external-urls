0c0a3974acb64ed910a5290fbe55864a4d04bf67
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2016-09-01 13:50:49 +0200

    add Python3 compatibility

--- external_urls/views.py
+++ external_urls/views.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import, division, print_function
 import base64
+import six
+
 from django.views.generic import RedirectView
+
 from external_urls.signals import external_click
 
 
@@ -9,6 +13,9 @@ class ExternalLinkView(RedirectView):
 
     def dispatch(self, request, external_url, *args, **kwargs):
         ip = request.META['REMOTE_ADDR']
-        self.url = base64.decodestring(external_url)
+        if six.PY2:
+            self.url = base64.decodestring(external_url)
+        elif six.PY3:
+            self.url = base64.decodebytes(bytes(external_url, 'utf-8')).decode('utf-8')
         external_click.send_robust(sender=self.__class__, url=self.url, ip=ip)
         return super(ExternalLinkView, self).dispatch(request, *args, **kwargs)
